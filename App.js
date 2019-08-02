1dad5c261e3bd9521cf408010a3336d2704699d6
Alex Ruan <alexruan2222@gmail.com>
2019-08-02 16:41:59 -0400

    added base expo files and template navigation/swipe-gesture implementation
    the true initial commit

---
+++ App.js
@@ -0,0 +1,203 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+
+
+/*Dependencies
+react-navigation
+react-native-swipe-gestures
+react-native-gesture-handler
+
+*/
+
+
+class HomeScreen extends React.Component {
+  render() {
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+    return (
+          <GestureRecognizer 
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+            onSwipeUp={this._onSwipeUp}
+          >
+            <Text>Home Screen</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => {
+                this.props.navigation.dispatch(StackActions.reset({
+                  index: 0,
+                  actions: [
+                    NavigationActions.navigate({ routeName: 'Details' })
+                  ],
+                }))
+              }}
+            />
+          </GestureRecognizer>
+      /*
+      */
+    );
+  }s
+  _onSwipeUp = gestureState =>{
+    this.props.navigation.dispatch(StackActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Details' })
+      ],
+    }))
+  }  
+}
+
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }  
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Details: {
+    screen: DetailsScreen,
+  },
+}, {
+    initialRouteName: 'Home',
+});
+
+export default createAppContainer(AppNavigator);
+
+/*Example code
+export default class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{height: 300}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
+*/
+
+
+
+/*'use strict';
+
+import React, { Component } from 'react';
+export default class App extends Component{
+  render() {
+    return React.createElement(Text, {style: styles.description}, "Search for houses to buy!");
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});*/
+
+
+/*import React, { Component } from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+class Greeting extends Component{
+  render(){
+    return(
+      <View style={{alignItems:'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+export default class App extends Component{
+  render(){
+    return(
+      <View style={{alignItems:'center', top: 50}}>
+          <Greeting name="Muji"/>
+          <Greeting name="Alex"/>
+          <Greeting name="Alan"/>
+      </View>
+    );
+  }
+}*/
+/*
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Blink extends Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
+*/
+
+/*
+Base app
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
